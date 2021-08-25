90ccdb63e8a8701d4c867900c9af337f06ec2ba6
FuruyaYuuki <yuki.furu0317@gmail.com>
2021-08-25 23:56:14 +0900

    add signup first

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyBA-DrMfqvw8GakCyxcoRQ6VeEFbCPXB_M",
+  authDomain: "ec-app-d38ff.firebaseapp.com",
+  projectId: "ec-app-d38ff",
+  storageBucket: "ec-app-d38ff.appspot.com",
+  messagingSenderId: "865554699567",
+  appId: "1:865554699567:web:8d56cb8593dad39704bdda",
+  measurementId: "G-BFV5PQ5GV7"
+};
